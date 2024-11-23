53874fb7cbf90ef8563f96547fa9cca39b4da38a
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2024-11-23 11:56:38 +0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
